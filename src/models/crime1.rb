ce3b5cc5ae161bacee3ca2382c61cfd75f8c7d51
linjinbo <obnijnil@gmail.com>
2023-10-11 23:04:49 +0800

    Fix reading match data for d1s in crime1

--- src/models/crime1.rb
+++ src/models/crime1.rb
@@ -54,7 +54,7 @@ class Crime1 < ActiveRecord::Base
         d7s = []
         sentences.each do |sentence|
           next if sentence.end_with?('的')
-          d1s << $~[1] if /(吸收|募集)[^#{Doc::PUNS}]*?([#{Doc::NUMS} ]+元)/.match(sentence)
+          d1s << $~[2] if /(吸收|募集)[^#{Doc::PUNS}]*?([#{Doc::NUMS} ]+元)/.match(sentence)
           d5s << $~[1] if /损失[^#{Doc::PUNS}]*?([#{Doc::NUMS} ]+元)/.match(sentence)
           case
           when /赔偿.*部分损失/.match?(sentence) then d7s << '部分赔偿'
